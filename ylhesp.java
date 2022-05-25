13549c7cc2a88a46fc1ecd51a785a8d134560b83
ismailcftc <105923196+ismailcftc@users.noreply.github.com>
2022-05-25 12:36:27 +0300

    Create ylhesp.java

---
+++ ylhesp.java
@@ -0,0 +1,22 @@
+public static void main(String[] args) {
+    int year;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Doğum Yılınızı Giriniz: ");
+    year = input.nextInt();
+
+    if(year%4==0){
+        if(year%100==0){
+            if(year%400==0){
+                System.out.println(year + "bir artık yıldır!");
+
+            } else{
+                System.out.println(year + "bir artık yıl değildir!");
+            }
+        } else{
+            System.out.println(year + "bir artık yıldır!");
+
+        }
+    } else{
+        System.out.println(year + "bir artık yıl degil !");
+    }
+}
