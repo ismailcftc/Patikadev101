4ec00f7acc968da7b3ed2f7b14d6639111e2f882
ismailcftc <105923196+ismailcftc@users.noreply.github.com>
2022-06-04 21:14:04 +0300

    Create factoriel.java

---
+++ factoriel.java
@@ -0,0 +1,21 @@
+package kodbloklari.com;
+ 
+import java.util.Scanner;
+ 
+public class Faktöriyel {
+    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        int faktoriyel = 1;
+        
+        System.out.println("Bir sayi giriniz: ");
+        int sayi = scan.nextInt();
+        
+        for(int i = 1; i <= sayi; i++)
+        {
+            faktoriyel = faktoriyel * i;
+        }
+        System.out.println(sayi + "!= " + faktoriyel);
+    }
+}
